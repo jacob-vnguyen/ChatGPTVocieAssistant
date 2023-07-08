0413b6ead27e6b426dc33822973057f705e74d8f
Jacob Nguyen <nguyenvjacob@gmail.com>
2023-07-07 19:14:23 -0600

    Finished project

---
+++ Voice_GPT3.py
@@ -0,0 +1,62 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+
+#Your open api key here
+openai.api_key = ""
+
+engine = pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print("Skipping Unknown Error")
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 4000,
+        n = 1,
+        stop = None,
+        temperature = 0.5,
+    )
+    return response["choices"][0]["text"]
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    while True:
+        print("Say 'Genius to start recoridng your question...")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "genius":
+                    filename = "input.wav"
+                    print("Say your question: ")
+                    with sr.Microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(filename, "wb") as f:
+                            f.write(audio.get_wav_data())
+                        text = transcribe_audio_to_text(filename)
+                        if text:
+                            print("You said: {text}")
+                            response = generate_response(text)
+                            print("GPT-3 says: {response}")
+
+                            speak_text(response)
+            except Exception as e:
+                print("An error occured: {}".format(e))
+
+if __name__ == "__main__":
+    main()
